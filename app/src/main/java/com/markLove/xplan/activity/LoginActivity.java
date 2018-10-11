fa0acbeaaa8d8afb041684c348695ce86af9bbce
lingjunHuang <lingjunhuang@snsshop.net>
2018-10-11 18:33:38 +0800

    更新提交

--- app/src/main/java/com/markLove/xplan/activity/LoginActivity.java
+++ app/src/main/java/com/markLove/xplan/activity/LoginActivity.java
@@ -1,14 +1,23 @@
 package com.markLove.xplan.activity;
 
-import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.ValueCallback;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.markLove.xplan.R;
 import com.markLove.xplan.base.mvp.BasePresenter;
-import com.markLove.xplan.base.ui.BaseActivity;
 
-public class LoginActivity extends BaseActivity implements View.OnClickListener{
+public class LoginActivity extends BaseContractActivity {
+    private WebView mWebView;
+    private ValueCallback<Uri> mUploadMessage;
+    private ValueCallback<Uri[]> mUploadCallbackAboveL;
 
     @Override
     protected int getContentViewId() {
@@ -17,8 +26,89 @@ public class LoginActivity extends BaseActivity implements View.OnClickListener{
 
     @Override
     protected void init(Bundle savedInstanceState) {
-        findViewById(R.id.btn_login).setOnClickListener(this);
-        findViewById(R.id.btn_register).setOnClickListener(this);
+        mWebView = new WebView(getApplicationContext());
+        LinearLayout mll = findViewById(R.id.rootView);
+        //避免内存泄露，采用动态添加的方式
+        mll.addView(mWebView);
+        initWebSettings();
+    }
+
+    /**
+     * 设置websetting
+     */
+    private void initWebSettings(){
+        WebSettings settings = mWebView.getSettings();
+        settings.setUseWideViewPort(true);
+        settings.setLoadWithOverviewMode(true);
+        settings.setDomStorageEnabled(true);
+        settings.setDefaultTextEncodingName("UTF-8");
+        settings.setAllowContentAccess(true); // 是否可访问Content Provider的资源，默认值 true
+        settings.setAllowFileAccess(true);    // 是否可访问本地文件，默认值 true
+        // 是否允许通过file url加载的Javascript读取本地文件，默认值 false
+        settings.setAllowFileAccessFromFileURLs(false);
+        // 是否允许通过file url加载的Javascript读取全部资源(包括文件,http,https)，默认值 false
+        settings.setAllowUniversalAccessFromFileURLs(false);
+        //开启JavaScript支持
+        settings.setJavaScriptEnabled(true);
+        // 支持缩放
+        settings.setSupportZoom(true);
+
+        mWebView.setWebChromeClient(new WebChromeClient(){
+            // For Android 3.0-
+            public void openFileChooser(ValueCallback<Uri> uploadMsg) {
+                mUploadMessage = uploadMsg;
+                showPicDialog();
+            }
+
+            // For Android 3.0+
+            public void openFileChooser(ValueCallback uploadMsg, String acceptType) {
+                mUploadMessage = uploadMsg;
+                showPicDialog();
+            }
+
+            //For Android 4.1
+            public void openFileChooser(ValueCallback<Uri> uploadMsg, String acceptType, String capture) {
+                mUploadMessage = uploadMsg;
+                showPicDialog();
+            }
+
+            // For Android 5.0+
+            public boolean onShowFileChooser(WebView webView, ValueCallback<Uri[]> filePathCallback, FileChooserParams fileChooserParams) {
+                mUploadCallbackAboveL = filePathCallback;
+                showPicDialog();
+                return true;
+            }
+        });
+
+        testFun();
+    }
+
+    private void testFun(){
+        findViewById(R.id.tv_test).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showPicDialog();
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (mWebView.canGoBack()){
+            mWebView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mWebView.removeAllViews();
+        mWebView.stopLoading();
+        mWebView.clearHistory();
+        mWebView.clearCache(true);
+        mWebView.destroy();
     }
 
     @Override
@@ -27,15 +117,14 @@ public class LoginActivity extends BaseActivity implements View.OnClickListener{
     }
 
     @Override
-    public void onClick(View v) {
-        switch (v.getId()){
-            case R.id.btn_login:
-                Intent intent = new Intent(this,MainActivity.class);
-                startActivity(intent);
-                break;
-            case R.id.btn_register:
-                Intent intent2 = new Intent(this,MainActivity.class);
-                break;
-        }
+    public void setBitmap(String imagePath) {
+//        Uri uri = Uri.fromFile(new File(imagePath));
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+//            mUploadCallbackAboveL.onReceiveValue(new Uri[]{uri});
+//        } else {
+//            mUploadMessage.onReceiveValue(uri);
+//        }
+        ImageView imageView = findViewById(R.id.iv_test);
+        imageView.setImageBitmap(BitmapFactory.decodeFile(imagePath));
     }
 }
---
+++ app/src/main/java/com/markLove/xplan/utils/Ln.java
@@ -0,0 +1,316 @@
+package com.markLove.xplan.utils;
+
+import android.app.Application;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+import com.markLove.xplan.BuildConfig;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class Ln {
+    private static Context context;
+    private static String pckName;
+
+    public static class BaseConfig {
+        protected int minimumLogLevel = BuildConfig.DEBUG ? Log.VERBOSE : Log.ASSERT;
+//        protected int minimumLogLevel = Log.VERBOSE;
+
+        protected String packageName = "";
+
+        protected String scope = "";
+
+        protected BaseConfig() {
+
+        }
+
+        public BaseConfig(Application context) {
+            try {
+                packageName = context.getPackageName();
+                scope = packageName.toUpperCase();
+
+                Ln.d("Configuring Logging, minimum log level is %s", logLevelToString(minimumLogLevel));
+
+            } catch (Exception e) {
+                Log.e(packageName, "Error configuring logger", e);
+            }
+        }
+
+        public int getLoggingLevel() {
+            return minimumLogLevel;
+        }
+
+        public void setLoggingLevel(int level) {
+            minimumLogLevel = level;
+        }
+    }
+
+    /**
+     * Default implementation logs to android.util.Log
+     */
+    public static class Print {
+        public int println(int priority, String msg) {
+            return Log.println(priority, getScope(5), processMessage(msg));
+        }
+
+        public int broadcast(int priority, String msg) {
+            if (context != null) {
+                try {
+                    Intent i = new Intent();
+                    i.setAction("com.gaopeng.wkd");
+                    i.putExtra("package", pckName);
+                    i.putExtra("priority", priority);
+                    i.putExtra("msg", msg);
+                    context.sendBroadcast(i);
+                } catch (Throwable t) {
+                }
+            }
+            return 0;
+        }
+
+        protected String processMessage(String msg) {
+            if (config.minimumLogLevel <= Log.DEBUG)
+                msg = String.format("%s %s", Thread.currentThread().getName(), msg);
+            return msg;
+        }
+
+        protected static String getScope(int skipDepth) {
+            if (config.minimumLogLevel <= Log.DEBUG) {
+                StackTraceElement[] stackTrace = Thread.currentThread().getStackTrace();
+                if (skipDepth >= 0 && skipDepth < stackTrace.length) {
+                    final StackTraceElement trace = stackTrace[skipDepth];
+                    String fileName = trace.getFileName();
+                    if (fileName == null || fileName.length() <= 0) {
+                        fileName = trace.getClassName();
+                    } else {
+                        fileName = config.scope + "/" + fileName;
+                    }
+
+                    int lineNum = trace.getLineNumber();
+                    String source = String.valueOf(lineNum);
+                    if (lineNum < 0) {
+                        source = trace.getMethodName();
+                        if (source == null || source.length() <= 0) {
+                            source = "Unknown Source";
+                        }
+                    }
+
+                    return fileName + "(" + source + ")";
+                }
+            }
+
+            return config.scope;
+        }
+
+    }
+
+    /**
+     * config is initially set to BaseConfig() with sensible defaults, then replaced by BaseConfig(ContextSingleton) during guice static injection pass.
+     */
+    protected static BaseConfig config = new BaseConfig();
+    /**
+     * print is initially set to Print(), then replaced by guice during static injection pass. This allows overriding where the log message is delivered to.
+     */
+    protected static Print print = new Print();
+
+    private Ln() {
+    }
+
+    public static void setContext(Context context) {
+        Ln.context = context.getApplicationContext();
+        Ln.pckName = context.getPackageName();
+    }
+
+    public static int v(Throwable t) {
+        return config.minimumLogLevel <= Log.VERBOSE ? print.println(Log.VERBOSE, Log.getStackTraceString(t)) : 0;
+    }
+
+    public static int v(Object format, Object... args) {
+        if (config.minimumLogLevel > Log.VERBOSE)
+            return 0;
+
+        final String s = format.toString();
+        final String message = args.length > 0 ? String.format(s, args) : s;
+        return print.println(Log.VERBOSE, message);
+    }
+
+    public static int v(Throwable throwable, Object format, Object... args) {
+        if (config.minimumLogLevel > Log.VERBOSE)
+            return 0;
+
+        final String s = format.toString();
+        final String message = (args.length > 0 ? String.format(s, args) : s) + '\n' + Log.getStackTraceString(throwable);
+        return print.println(Log.VERBOSE, message);
+    }
+
+    public static int d(Throwable t) {
+        return config.minimumLogLevel <= Log.DEBUG
+                ? print.println(Log.DEBUG, Log.getStackTraceString(t))
+                : print.broadcast(Log.DEBUG, Log.getStackTraceString(t));
+    }
+
+    public static int d(Object format, Object... args) {
+        if (config.minimumLogLevel > Log.DEBUG) {
+            final String s = format.toString();
+            final String message = args.length > 0 ? String.format(s, args) : s;
+            return print.broadcast(Log.DEBUG, message);
+        }
+
+        final String s = format.toString();
+        final String message = args.length > 0 ? String.format(s, args) : s;
+        return print.println(Log.DEBUG, message);
+    }
+
+    public static int d(Throwable throwable, Object format, Object... args) {
+        if (config.minimumLogLevel > Log.DEBUG) {
+            final String s = format.toString();
+            final String message = (args.length > 0 ? String.format(s, args) : s) + '\n' + Log.getStackTraceString(throwable);
+            return print.broadcast(Log.DEBUG, message);
+        }
+
+        final String s = format.toString();
+        final String message = (args.length > 0 ? String.format(s, args) : s) + '\n' + Log.getStackTraceString(throwable);
+        return print.println(Log.DEBUG, message);
+    }
+
+    public static int i(Throwable t) {
+        return config.minimumLogLevel <= Log.INFO
+                ? print.println(Log.INFO, Log.getStackTraceString(t))
+                : print.broadcast(Log.INFO, Log.getStackTraceString(t));
+    }
+
+    public static int i(Object format, Object... args) {
+        if (config.minimumLogLevel > Log.INFO) {
+            final String s = format.toString();
+            final String message = args.length > 0 ? String.format(s, args) : s;
+            return print.broadcast(Log.INFO, message);
+        }
+
+        final String s = format.toString();
+        final String message = args.length > 0 ? String.format(s, args) : s;
+        return print.println(Log.INFO, message);
+    }
+
+    public static int i(Throwable throwable, Object format, Object... args) {
+        if (config.minimumLogLevel > Log.INFO) {
+            final String s = format.toString();
+            final String message = (args.length > 0 ? String.format(s, args) : s) + '\n' + Log.getStackTraceString(throwable);
+            return print.broadcast(Log.INFO, message);
+        }
+
+        final String s = format.toString();
+        final String message = (args.length > 0 ? String.format(s, args) : s) + '\n' + Log.getStackTraceString(throwable);
+        return print.println(Log.INFO, message);
+    }
+
+    public static int w(Throwable t) {
+        return config.minimumLogLevel <= Log.WARN ? print.println(Log.WARN, Log.getStackTraceString(t)) : 0;
+    }
+
+    public static int w(Object format, Object... args) {
+        if (config.minimumLogLevel > Log.WARN)
+            return 0;
+
+        final String s = format.toString();
+        final String message = args.length > 0 ? String.format(s, args) : s;
+        return print.println(Log.WARN, message);
+    }
+
+    public static int w(Throwable throwable, Object format, Object... args) {
+        if (config.minimumLogLevel > Log.WARN)
+            return 0;
+
+        final String s = format.toString();
+        final String message = (args.length > 0 ? String.format(s, args) : s) + '\n' + Log.getStackTraceString(throwable);
+        return print.println(Log.WARN, message);
+    }
+
+    public static int e(Throwable t) {
+        return config.minimumLogLevel <= Log.ERROR
+                ? print.println(Log.ERROR, Log.getStackTraceString(t))
+                : print.broadcast(Log.ERROR, Log.getStackTraceString(t));
+    }
+
+    public static int e(Object format, Object... args) {
+        if (config.minimumLogLevel > Log.ERROR) {
+            final String s = format.toString();
+            final String message = args.length > 0 ? String.format(s, args) : s;
+            return print.broadcast(Log.ERROR, message);
+        }
+
+        final String s = format.toString();
+        final String message = args.length > 0 ? String.format(s, args) : s;
+        return print.println(Log.ERROR, message);
+    }
+
+    public static int e(Throwable throwable, Object format, Object... args) {
+        if (config.minimumLogLevel > Log.ERROR) {
+            final String s = format.toString();
+            final String message = (args.length > 0 ? String.format(s, args) : s) + '\n' + Log.getStackTraceString(throwable);
+            return print.broadcast(Log.ERROR, message);
+        }
+
+        final String s = format.toString();
+        final String message = (args.length > 0 ? String.format(s, args) : s) + '\n' + Log.getStackTraceString(throwable);
+        return print.println(Log.ERROR, message);
+    }
+
+    public static int json(String json) {
+        return i(formatJSON(json));
+    }
+
+    public static String formatJSON(String json) {
+        String formattedString = json;
+        if (json == null || json.trim().length() == 0) {
+            throw new RuntimeException("JSON empty.");
+        }
+        try {
+            if (json.startsWith("{")) {
+                JSONObject jsonObject = new JSONObject(json);
+                formattedString = jsonObject.toString(4);
+            } else if (json.startsWith("[")) {
+                JSONArray jsonArray = new JSONArray(json);
+                formattedString = jsonArray.toString(4);
+            } else {
+                Ln.e("JSON should start with { or [, but found " + json);
+            }
+        } catch (Exception e) {
+            Ln.e(e);
+        }
+        return formattedString;
+    }
+
+    public static boolean isDebugEnabled() {
+        return config.minimumLogLevel <= Log.DEBUG;
+    }
+
+    public static boolean isVerboseEnabled() {
+        return config.minimumLogLevel <= Log.VERBOSE;
+    }
+
+    public static BaseConfig getConfig() {
+        return config;
+    }
+
+    public static String logLevelToString(int loglevel) {
+        switch (loglevel) {
+            case Log.VERBOSE:
+                return "VERBOSE";
+            case Log.DEBUG:
+                return "DEBUG";
+            case Log.INFO:
+                return "INFO";
+            case Log.WARN:
+                return "WARN";
+            case Log.ERROR:
+                return "ERROR";
+            case Log.ASSERT:
+                return "ASSERT";
+        }
+
+        return "UNKNOWN";
+    }
+
+}
